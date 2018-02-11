795112e6ec497acdf8db1db8fa774dc2374ea046
iggor <iggorbeni@gmail.com>
2018-02-11 19:42:58 -0200

    Reformat code

--- src/main/java/br/com/igbeni/redventuresbackend/config/security/JWTLoginFilter.java
+++ src/main/java/br/com/igbeni/redventuresbackend/config/security/JWTLoginFilter.java
@@ -1,6 +1,24 @@
+/*
+ * Copyright  (C) 2018 Iggor Benicio A. S. Alves.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Iggor Benicio A. S. Alves
+ */
+
 package br.com.igbeni.redventuresbackend.config.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -9,12 +27,10 @@ import org.springframework.security.web.authentication.AbstractAuthenticationPro
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Map;
 
 public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
 
